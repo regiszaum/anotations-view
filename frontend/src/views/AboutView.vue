af5f56c7e68dea90b0efb9a85eeca463356f8002
regiszaum <regisf97@gmail.com>
2024-01-20 00:19:35 -0300

    initial project setup

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-100">
+    <div class="container">
+      <h1>Ae</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
